e1db0439a237153b20814b9cd4eb274b2fe4656a
Jean-Christophe <jean-christophe.benoit@opendigitaleducation.com>
2021-03-15 16:56:10 +0100

    hack: exposing commonjs module exports as global namespace for use in browsers

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -4,3 +4,13 @@ export * as Samples from './samples/index';
 /* Other directives are exported directly, for simplicity. */
 export * from './directives/index';
 // TODO add other directives here...
+
+
+
+/** Hack pour rendre compatible le module commonjs avec une balise <script> dans un navigateur. */
+declare var window:any;
+declare var module:any;
+if(typeof window!=='undefined') {
+    window.entcore = window.entcore||{};
+    window.entcore["ode-ngjs-front"] = module.exports;
+}
