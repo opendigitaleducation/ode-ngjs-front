e0b835ac58d5d25a62bc0e0af1225f731694da70
Jean-Christophe <jean-christophe.benoit@opendigitaleducation.com>
2021-03-24 16:44:59 +0100

    fix: simplification of items & folders list

--- src/ts/directives/index.ts
+++ src/ts/directives/index.ts
@@ -3,5 +3,3 @@ export * as Explorer from './explorer/explorer.directive';
 export * as Sidebar from './sidebar/sidebar.directive';
 export * as Folder from './sidebar/folder.directive';
 export * as ResourceList from './resource-list/resource-list.directive';
-export * as ListFolder from './resource-list/list-folder.directive';
-export * as ListItem from './resource-list/list-item.directive';
--- src/ts/modules/ExplorerModule.ts
+++ src/ts/modules/ExplorerModule.ts
@@ -1,11 +1,9 @@
 import angular from "angular";
-import { Explorer, Folder, ListFolder, ListItem, ResourceList, Sidebar } from "../directives";
+import { Explorer, Folder, ResourceList, Sidebar } from "../directives";
 
 angular.module("odeExplorerModule", [])
 .directive("odeExplorer", Explorer.DirectiveFactory)
 .directive("odeSidebar", Sidebar.DirectiveFactory)
 .directive("odeFolder", Folder.DirectiveFactory)
 .directive("odeResourceList", ResourceList.DirectiveFactory)
-.directive("odeListItem", ListItem.DirectiveFactory)
-.directive("odeListFolder", ListFolder.DirectiveFactory)
 ;
