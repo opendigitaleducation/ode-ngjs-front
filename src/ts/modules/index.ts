891c8c3df6631a69ac11c98bb14e0179d149da50
Jean-Christophe <jean-christophe.benoit@opendigitaleducation.com>
2021-05-20 14:12:30 +0200

    fix: refactoring for less code

--- src/ts/modules/index.ts
+++ src/ts/modules/index.ts
@@ -1,5 +1,5 @@
 /* TODO */
-export * as BaseModule from './base.module';
-export * as IdiomModule from './i18n.module';
-export * as UiModule from './ui.module';
-export * as ExplorerModule from './explorer.module';
+export * from './base.module';
+export * from './i18n.module';
+export * from './ui.module';
+export * from './explorer.module';
