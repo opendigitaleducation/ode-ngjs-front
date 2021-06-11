93a7556d15f7579e40e594e2efa2cebc34010b51
Jean-Christophe <jean-christophe.benoit@opendigitaleducation.com>
2021-06-11 11:44:39 +0200

    fix: app-title directive cannot bind to controller

--- src/ts/directives/components/app-title/app-title.directive.ts
+++ src/ts/directives/components/app-title/app-title.directive.ts
@@ -1,31 +1,10 @@
 import { IAttributes, IController, IDirective, IScope } from "angular";
 
-/* Controller for the directive */
-export class Controller implements IController {
-    constructor() {
-    }
-}
-
 /* Directive */
 class Directive implements IDirective<IScope,JQLite,IAttributes,IController[]> {
     restrict = 'A';
-    scope = {
-    };
-	bindToController = true;
-	controller = [Controller];
-	controllerAs = 'ctrl';
-	require = ['appTitle'];
 
-    /**
-     * Link method for the directive.
-     * @see https://code.angularjs.org/1.7.9/docs/api/ng/service/$compile
-     * @param $scope scope
-     * @param $elem jqLite-wrapped element that this directive matches.
-     * @param $attr hash object with key-value pairs of normalized attribute names and their corresponding attribute values.
-     * @param controllers Array of "require"d controllers : [ngModelCtrl]
-     */
     link(scope:IScope, elem:JQLite, attr:IAttributes, controllers:IController[]|undefined): void {
-        let ctrl:Controller|null = controllers ? controllers[0] as Controller : null;
         // NOTE : previous code to adapt if required:
         // elem.addClass('zero-mobile');
         // elem.find('h1').addClass('application-title');
@@ -47,10 +26,6 @@ class Directive implements IDirective<IScope,JQLite,IAttributes,IController[]> {
         // scope.$on("$destroy", function() {
         //     $('body').find('header.main .application-title').remove();
         // });
-    }
-
-    /* Constructor with Dependency Injection */
-    constructor() {
     }
 }
 
