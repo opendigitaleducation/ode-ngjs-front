0e596a426f2c7fb2ec866b6b6760d019ed30ca9c
Jean-Christophe <jean-christophe.benoit@opendigitaleducation.com>
2021-08-04 12:12:11 +0200

    feat: added a size attribute to the ode-modal directive (sm, lg or xl)

--- src/ts/directives/components/modal/modal.directive.ts
+++ src/ts/directives/components/modal/modal.directive.ts
@@ -1,25 +1,34 @@
 import { IController, IDirective } from "angular";
 
+type ModalSize = "sm"|"lg"|"xl";
+
 /* Controller for the directive */
 export class Controller implements IController {
 	constructor() {
         // Remove transpilation warnings due to the "bindToController", which angularjs already checks.
 		this.id = null as unknown as string;
 		this.onValidate = null as unknown as Function;
+		this.size = null;
 	}
 	id:string;
+	size:ModalSize|null;
 	visible?:boolean;
 	onClose?:Function;
 	onValidate:Function;
 
-	getClass() {
-		return {show:this.visible};
-	}
 	getStyle() {
 		return {
-			 display: this.visible ? 'block' : 'none'
+			 "display": this.visible ? 'block' : 'none'
 			,"background-color": 'transparent'
 		};
+	}
+	getSizeClass() {
+		switch( this.size ) {
+			case "sm": return "modal-sm";
+			case "lg": return "modal-lg";
+			case "xl": return "modal-xl";
+			default: return "";
+		}
 	}
 }
 
@@ -29,6 +38,7 @@ class Directive implements IDirective {
 	template = require('./modal.directive.html').default;
 	scope = {
 		id:"@",
+		size: "@?",
 		visible:"=?",
 		onClose:"&?"
 	};
@@ -45,7 +55,7 @@ class Directive implements IDirective {
 /** The ode-modal directive.
  *
  * Usage:
- *      &lt;ode-modal>
+ *      &lt;ode-modal size?="sm|lg|xl">
  *        &lt;ode-modal-title>Your HTML title here</ode-modal-title>
  *        &lt;ode-modal-body>Your HTML content here</ode-modal-body>
  *        &lt;ode-modal-footer>Your HTML footer here</ode-modal-footer>
