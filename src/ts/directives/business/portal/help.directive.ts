00aabe1ac635efbad2ff6f041e89be7c215eca64
clementcreusat <clement.creusat@opendigitaleducation.com>
2021-11-17 12:48:55 +0100

    fix: #50847, modal BS5 help js fixes

--- src/ts/directives/business/portal/help.directive.ts
+++ src/ts/directives/business/portal/help.directive.ts
@@ -23,8 +23,12 @@ export class Directive implements IDirective<Scope,JQLite,IAttributes> {
         } 
         let helpPath = await skin.getHelpPath();
         let helpText:string;
+        let helpErrorTitle:string;
+        let helpErrorText:string;
 
         const lang = conf().Platform.idiom;
+        const modalElement = $('ode-modal');
+        modalElement.find('.modal').addClass('modal-help');
 
         scope.onHelp = function() {
             if (helpText) {
@@ -33,14 +37,16 @@ export class Directive implements IDirective<Scope,JQLite,IAttributes> {
             else {
                 http().get<string>(
                     scope.helpPath,
-                    {queryParams:{"_": conf().Platform.deploymentTag}}
+                    {queryParams:{"index.html": conf().Platform.deploymentTag}}
                 ).then( content => {
                     if( http().latestResponse.status === 404 ) {
-                        helpText = '<h2>' + lang.translate('help.notfound.title') + '</h2><p>' + lang.translate('help.notfound.text') + '</p>';
+                        helpErrorTitle = lang.translate('help.notfound.title');
+                        helpErrorText = '<p>' + lang.translate('help.notfound.text') + '</p>';
+                        setErrorHTML(helpErrorTitle, helpErrorText);
                     } else {
                         helpText = content;
+                        setHtml(helpText);
                     }
-                    setHtml(helpText);
                     scope.$apply();
                 })
             }
@@ -61,37 +67,44 @@ export class Directive implements IDirective<Scope,JQLite,IAttributes> {
 
         let helpContent, burgerMenuElement:JQuery<HTMLElement>, burgerButtonElement;
 
-        var setHtml = function(content:string){
+        const setErrorHTML = function(title:string, text:string) {
+            modalElement.find('.modal-title').html(title);
+            modalElement.find('.modal-body').html(text);
+            scope.display.read = true;
+            scope.$apply('display');
+        }
+
+        const setHtml = function(content:string){
             helpContent = $('<div>' + content + '</div>');
             // Swap ToC and introduction paragraphs
             helpContent.find('> p').prev().insertAfter(helpContent.find('> p'));
             helpContent.find('img').each(function(index, item){
                 $(item).attr('src', scope.helpPath + "../.." + $(item).attr('src'));
             });
-            element.find('ode-modal-body > div').html(helpContent.html());
-            element.find('li a').on('click', function(e){
-                element.find('.section').slideUp();
+            modalElement.find('ode-modal-body > div').html(helpContent.html());
+            modalElement.find('li a').on('click', function(e){
+                modalElement.find('.section').slideUp();
                 $('div#' + $(e.target).attr('href')?.split('#')[1]).slideDown();
             });
-            element.find('div.paragraph a').on('click', function(e){
+            modalElement.find('div.paragraph a').on('click', function(e){
                 window.open($(e.target).closest('a').attr('href'), "_newtab" ); 
             });
-            element.find('li a').first().click();
+            modalElement.find('li a').first().click();
 
             // Activate hamburger menu on responsive
-            element.find('#TOC').wrap('<div id="burger-menu" class="burger-menu"></div>');
-            burgerMenuElement = element.find('#burger-menu');
+            modalElement.find('#TOC').wrap('<div id="burger-menu" class="burger-menu"></div>');
+            burgerMenuElement = modalElement.find('#burger-menu');
             burgerMenuElement.prepend('<button id="burger-button" class="burger-button"><i class="burger-icon"></i></button>');
             burgerButtonElement = element.find('#burger-button');
             burgerButtonElement.click(function(e) {
                 burgerMenuElement.toggleClass('active');
             }); 
-            element.find('#TOC > ul li a').on('click', function (e) {
+            modalElement.find('#TOC > ul li a').on('click', function (e) {
                 burgerMenuElement.removeClass('active');
             });
             
             let bodyClick = function (event:any) {
-                if (element.find('#TOC > ul').find(event.target).length == 0 
+                if (modalElement.find('#TOC > ul').find(event.target).length == 0 
                     && burgerMenuElement.find(event.target).length == 0) {
                     burgerMenuElement.removeClass('active');
                 }
