145a070459497392b8b94d41c6aa63ab43670203
Jean-Christophe <jean-christophe.benoit@opendigitaleducation.com>
2022-03-21 18:31:16 +0100

    feat(explorer): #WB-341, front to back POC

--- src/ts/directives/business/explorer/sidebar.directive.ts
+++ src/ts/directives/business/explorer/sidebar.directive.ts
@@ -1,8 +1,8 @@
 import * as Explorer from './explorer.directive';
 import { IAttributes, IController, IDirective, IScope } from "angular";
-import { GetResourcesResult } from "ode-ts-client";
 import { SearchStore } from "../../../stores/search.store";
 import { FolderController } from "./sidebar-folder.directive";
+import { NgHelperService } from '../../../services';
 
 /* Controller for the directive */
 export class Controller implements IController {
@@ -11,17 +11,43 @@ export class Controller implements IController {
         this.model = null as unknown as SearchStore;
     }
     model: SearchStore;
+	display= {
+		showNewFolder: false
+	};
+	folderName = "";
+
+    // Shortcut for updating the view.
+	public requestUpdate?:()=>void;
 
 	onSelectFolder(folderCtrl:FolderController):void {
 		this.model.searchParameters.filters.folder = folderCtrl.folder?.id;
-		this.model.explorer.getResources().then( (result:GetResourcesResult) => {
+		this.model.explorer.getResources()
+		.then( result => {
 			folderCtrl.subfolders = result.folders;
 		});
+	}
+
+	onCreateFolder() {
+		if( typeof this.folderName==="string" && this.folderName.trim().length ) {
+			this.model.explorer.createFolder( this.model.resourceType, this.model.currentFolder?.id ?? "default", this.folderName.trim() )
+			.then( r => {
+				this.model.loadedFolders.push( r );
+				return r;
+			})
+			.then( r => {
+				this.display.showNewFolder = false;
+				this.requestUpdate && this.requestUpdate();
+			});
+		}
 	}
 }
 
 /* Directive */
 class Directive implements IDirective<IScope,JQLite,IAttributes,IController[]> {
+    constructor(
+        private helperSvc:NgHelperService
+    ) {}
+
     restrict = 'E';
 	template = require('./sidebar.directive.html').default;
 	scope = {};
@@ -35,7 +61,11 @@ class Directive implements IDirective<IScope,JQLite,IAttributes,IController[]> {
 		if( !controllers ) return;
 		const ctrl:Controller = controllers[0] as Controller;
 		const odeExplorer:Explorer.Controller = controllers[1] as Explorer.Controller;
+
 		ctrl.model = odeExplorer.model;
+		ctrl.requestUpdate = () => {
+			this.helperSvc.safeApply( scope );   // Force reevaluation of the recorder's field
+		};
 	}
 }
 
@@ -44,6 +74,7 @@ class Directive implements IDirective<IScope,JQLite,IAttributes,IController[]> {
  * Usage:
  *      &lt;ode-sidebar></ode-sidebar&gt;
  */
-export function DirectiveFactory() {
-	return new Directive();
+export function DirectiveFactory(helperSvc: NgHelperService) {
+	return new Directive(helperSvc);
 }
+DirectiveFactory.$inject = ["odeNgHelperService"];
