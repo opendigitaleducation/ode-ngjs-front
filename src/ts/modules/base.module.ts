9130408116a8e6b4fe331648adc4f15ff73b505f
david-cc <david.caudeli@web-education.net>
2022-11-17 17:27:17 +0100

    fix: #WB-1330, add  whitelist configuration to fix CDN template loading

--- src/ts/modules/base.module.ts
+++ src/ts/modules/base.module.ts
@@ -1,13 +1,22 @@
 import angular, { IInterpolateProvider, IModule } from "angular";
 import { NgHelperService, NotifyService, SessionService, TrackingService, notify, XitiService } from "../services";
+import { ConfigurationFrameworkFactory } from "ode-ts-client";
 
 const module = angular.module("odeBase", 
     ['ngSanitize', 'ngRoute'], 
     ['$interpolateProvider', function($interpolateProvider:IInterpolateProvider) {
         $interpolateProvider.startSymbol('[[');
         $interpolateProvider.endSymbol(']]');
-    }]
-);
+    }])
+    // FIX #WB-1330: Add $sce whitelist configuration to fix CDN template loading
+    .config(['$sceDelegateProvider', function($sceDelegateProvider: any) {
+		$sceDelegateProvider.resourceUrlWhitelist([
+			// Allow same origin resource loads.
+			'self',
+			// Allow loading from our assets domain. **.
+			ConfigurationFrameworkFactory.instance().Platform.cdnDomain + '/**'
+        ])
+    }]);
 
 /**
  * The "odeBase" angularjs module provides basic initialization features to other modules (apps).
